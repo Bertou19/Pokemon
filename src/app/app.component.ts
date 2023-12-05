8b3480a42996efc296ef777f252f1aedda436b41
Bertou19 <a_bertille@msn.comm>
2023-12-05 15:06:31 +0100

    mise en place des routes, détails pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterOutlet } from "@angular/router";
-//On vient importer la constante avec les données
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -11,38 +6,4 @@ import { Pokemon } from "./pokemon";
   //On permet d'afficher la vue correspondante
   templateUrl: "app.component.html",
 })
-export class AppComponent implements OnInit {
-  //On va chercher la constante POKEMON, on la place dans la variable et on type avec Typescript
-  //pour éviter de pouvoir entrer des données en dur
-  pokemonList: Pokemon[] = POKEMONS;
-  //On affiche le pokemon selectionné, il va être poussé avec l'interpolation dans le template
-  //On met undefined qui il peut ne pas exister dans la const pokemon methode selectPokemon
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    //On converti le pokemonId en un nombre
-    const id = +pokemonId;
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      //On passe une fonction pour voir si dans la liste il y a un pokemon qui a l'id qu'on recherche
-      //On converti la string en nombre
-      (pokemon) => pokemon.id == +pokemonId
-    );
-    if (pokemon) {
-      console.log(`Vous avez demandé le pokemon ${pokemon.name}`);
-      //On initilise la méthode pokemonSelected
-      //On la met à jour
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokemon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
-//L'utilisateur saisi un nombre dans le template, celui ci est détecté cote composant par la méthode selectPokemon,
-//qui va mettre à jour dans la méthode pokemonSelected,
-//ensuite cette propriété est poussée dans le template grâce à l'interpolation au niveau pokemonselected
-//
+export class AppComponent {}
---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { POKEMONS } from "../mock-pokemon-list";
+import { Pokemon } from "../pokemon";
+@Component({
+  selector: "app-detail-pokemon",
+  templateUrl: "./detail-pokemon.component.html",
+  styles: ``,
+})
+export class DetailPokemonComponent implements OnInit {
+  pokemonList: Pokemon[] | undefined;
+  pokemon: Pokemon | undefined; //propriété qui contient un pokemon à afficher
+
+  //On injecte le service ActivatedRoute pour aller chercher l'information dont on a besoin à partir de l'url
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    //Aller chercher l'id contenue dans l'url
+    const pokemonId: string | null = this.route.snapshot.paramMap.get("id");
+    //Si il existe on attribue à la propriété Pokemon, l'id qui correspond à cet identifiant.
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(
+        (pokemon) => pokemon.id == +pokemonId
+      );
+    }
+  }
+}
